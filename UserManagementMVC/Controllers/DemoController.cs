52151777baf54eeaaf6a475cd9fecb60f2a83403
John <John@DESKTOP-IN2PUCM>
2017-08-07 04:12:54 +0800

    Entity Framework connects to the database

--- UserManagementMVC/Controllers/DemoController.cs
+++ UserManagementMVC/Controllers/DemoController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using UserManagementMVC.DatabaseContext;
 using UserManagementMVC.Helper;
 using UserManagementMVC.Models;
 
@@ -29,6 +32,45 @@ namespace UserManagementMVC.Controllers
             Logger.Debug($"End --- {DateTime.Now}");
 
             return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        private int Add(User user)
+        {
+            using (var db = new AzureContext())
+            {
+                db.User.Add(user);
+                return db.SaveChanges();
+            }
+        }
+
+        private int Delete(int userId)
+        {
+            using (var db = new AzureContext())
+            {
+                var user = Select(userId);
+                if (user != null)
+                {
+                    db.User.Remove(user);
+                }
+                return db.SaveChanges();
+            }
+        }
+
+        private int Update(User user)
+        {
+            using (var db = new AzureContext())
+            {
+                db.User.Attach(user);
+                return db.SaveChanges();
+            }
+        }
+
+        private User Select(int userId)
+        {
+            using (var db = new AzureContext())
+            {
+                return db.User.FirstOrDefault(x => x.UserId == userId);
+            }
         }
     }
 }
---
+++ UserManagementMVC/DatabaseContext/AzureContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using UserManagementMVC.Models;
+
+namespace UserManagementMVC.DatabaseContext
+{
+    public class AzureContext : DbContext
+    {
+        public AzureContext() : base("name=DefaultConnection") { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
